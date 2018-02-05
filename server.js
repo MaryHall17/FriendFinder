00f5c4105bf4f1c77d1c4d4473ed09a6deb8de2b
Mary Hall <mh6uj@students-MBP.home>
2018-02-04 21:15:05 -0500

    initial server logic

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+//Dependncies 
+
 var express = require("express");
 
 var bodyParser = require("body-parser");
 
 var path = require("path");
 
-console.log("Express is linked: " + express);
+//Express app
 
-console.log("Body parser is linked: " + bodyParser);
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-console.log("Path is linked: " + path);
+console.log("App is listening: " + PORT);
+
+//Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(_dirname + '/public'))
 
