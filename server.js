ff2738efd356ff7cff2b60b38e78ee7c2c28edde
Mary Hall <mh6uj@students-MBP.home>
2018-02-04 21:10:17 -0500

    packages successfully linked

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var path = require("path");
+
+console.log("Express is linked: " + express);
+
+console.log("Body parser is linked: " + bodyParser);
+
+console.log("Path is linked: " + path);
+
