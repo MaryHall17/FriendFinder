ba34fc70df3cfa7e43c727c6ae768394392abd3a
Mary Hall <mh6uj@students-MBP.home>
2018-02-06 14:27:01 -0500

    successfully pushing friend object to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,28 @@
 //Friends object
 
-// var Friends = function() 
-// 	{
-// 		this.name = $("#friend-name").val(),
-// 		this.photo = $("#photo-link").val(),
-// 		this.score = [$("#Q1").val(), 
-// 					$("#Q2").val(),
-// 					$("#Q3").val(),
-// 					$("#Q4").val(),
-// 					$("#Q5").val(),
-// 					$("#Q6").val(),
-// 					$("#Q7").val(),
-// 					$("#Q8").val(),
-// 					$("#Q9").val(),
-// 					$("#Q10").val()
 
-// 				]
-// 	}
 
 $(document).ready(function() {
+	var friendsArray = [
+	{
+		name: "Chrom",
+		photo: "http://fireemblem.wikia.com/wiki/File:Chrom_Fates.png",
+		score: [1, 
+				5,
+				3,
+				5,
+				5,
+				5,
+				5,
+				1,
+				2,
+				5
+
+			]
+	}
+];
+
+console.log(friendsArray);
 	
 	$("#submit-survey").click(function(){
 		var Friends =  
@@ -38,6 +42,8 @@ $(document).ready(function() {
 
 				]
 			}
+		friendsArray.push(Friends);	
 		console.log(Friends);
+		console.log(friendsArray);
 	})
 });
